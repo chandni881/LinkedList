e0005640a0035648de0e5734f83787262bdd2c2e
chandni881 <cpatel4@umbc.edu>
2017-02-22 23:48:37 +0000

    LinkedList Class

---
+++ src/main/java/LinkedList/main.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+
+public class main {
+    public static void main( String[] args )
+    {
+        LinkedList ll = new LinkedList();
+        
+    }
+
+}
